b0cea1b485ce7fbf5128bc5edb7aa7367dac95be
Ayaan Kazerouni <ayaan@vt.edu>
2018-09-13 13:02:44 -0400

    Initial commit.

---
+++ src/org/webcat/deveventtracker/controllers/Driver.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.webcat.deveventtracker.controllers;
+
+/**
+ * Queries the Web-CAT DB for sensordata and populates a
+ * "drop-off point" with incremental development feedback.
+ * 
+ * @author Ayaan Kazerouni
+ * @version 2018-09-13
+ */
+public class Driver {
+
+	/**
+	 * Entry point for the application. Calculates incremental development
+	 * feedback for the specified student project.
+	 * 
+	 * Command line arguments should include a student id and an
+	 * assignment id.
+	 * 
+	 * @param args Command line arguments
+	 */
+	public static void main(String[] args) {
+		// No op for now
+	}
+}
