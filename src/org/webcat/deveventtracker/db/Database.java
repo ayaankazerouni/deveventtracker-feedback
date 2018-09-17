8b9238bb5a4d96646e4b0342bf88415345d5abbd
Ayaan Kazerouni <ayaan@vt.edu>
2018-09-16 21:21:40 -0400

    Tentative query

--- src/org/webcat/deveventtracker/db/Database.java
+++ src/org/webcat/deveventtracker/db/Database.java
@@ -4,8 +4,15 @@
 package org.webcat.deveventtracker.db;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.webcat.deveventtracker.models.SensorData;
 
 /**
  * Singleton class providing restricted access to
@@ -53,5 +60,37 @@ public class Database {
         }
         
         return theInstance;
+    }
+
+    public SensorData[] getEventsinTimeRange(String userId, String projectId, long startTime, long endTime) {
+        List<SensorData> events = new ArrayList<SensorData>();
+        
+        String stmt = "select SensorData.userId, SensorData.projectId, " +
+            "SensorData.time,  TASSIGNMENTOFFERING.`CDUEDATE` as deadline, " +
+            "SensorData.`currentSize`, SensorDataProperty.value as 'className'" +
+        "from `SensorData`, `SensorDataProperty`, `ProjectForAssignment`, " +
+            "`ProjectForAssignmentStudent`,`TASSIGNMENTOFFERING`" +
+        "where SensorData.`projectId` = ProjectForAssignment.`OID`" +
+            "and ProjectForAssignment.`assignmentOfferingId` = TASSIGNMENTOFFERING.`OID`" +
+            "and ProjectForAssignmentStudent.`projectId` = ProjectForAssignment.`OID`" +
+            "and SensorData.`userId` = ProjectForAssignmentStudent.`studentId`" +
+            "and SensorData.`userId` = ?" +
+            "and SensorData.`projectId` = ?" +
+            "and SensorData.`OID` = SensorDataProperty.`sensorDataId`" +
+            "and SensorDataProperty.`name` = 'Class-Name'" +
+            "and SensorData.time >= ?" + 
+            "and SensorData.time <= ?;";
+        try  {
+            PreparedStatement preparedStatement = this.connect.prepareStatement(stmt);
+            preparedStatement.setString(1, userId);
+            preparedStatement.setString(2, projectId);
+            preparedStatement.setDate(3, new Date(startTime));
+            preparedStatement.setDate(4, new Date(endTime));
+            ResultSet result = preparedStatement.executeQuery();
+        } catch(SQLException e) {
+            System.out.println("An error occured while executing the query.");
+        }
+
+        return events.toArray(new SensorData[events.size()]);
     }
 }
